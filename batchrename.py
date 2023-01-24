fa2e2e9db1473382a9055072ac15c5fdd114f203
ejkeenan <ejkeenan@gmail.com>
2023-01-24 09:12:51 -0800

    Update batchrename.py
    adding some comments

--- batchrename.py
+++ batchrename.py
@@ -4,15 +4,19 @@ print("Hello! This is a quick script to mass rename tasks in asana, please follo
 
 print("To get your access token go to https://developers.asana.com/docs/authentication-quick-start\n")
 
+#gets user to enter access token to make sure they have access
 MY_PERSONAL_ACCESS_TOKEN = input("PERSONAL ACCESS TOKEN: ")
 
 client = asana.Client.access_token(MY_PERSONAL_ACCESS_TOKEN)
 client.LOG_ASANA_CHANGE_WARNINGS = False
 
+#prints users name to confirm access/they are logged in correctly
 print("Access Token Accepted - Logged in as: " + client.users.me()['name'])
 
+#user input to select project to update
 print("\nTo find the Project ID - please copy the numbers here in any project - \nhttps://app.asana.com/0/################/list\n")
 
+#function to update tasks based on old text (find) and new text (replace).  Also provides user the total number of tasks updated
 def update_tasks():
   projectid = input("Project ID: ")
   project_name = client.projects.get_project(projectid)["name"]
